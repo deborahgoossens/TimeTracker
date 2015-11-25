255a54c0d5ffc761f2004c310731953d9921a4ce
Deborah Goossens <deborah@connectholland.nl>
2015-11-25 21:34:50 +0100

    Create User.java

---
+++ User.java
@@ -0,0 +1,19 @@
+/*
+ * User
+ */
+package timetracker;
+
+/**
+ *
+ * @author Deborah Goossens
+ */
+public class User {
+    
+    int id;
+    String name;
+    
+    public User(String name) {
+        this.name = name;
+        id = 1;        
+    }
+}
