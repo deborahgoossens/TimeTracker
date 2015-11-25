17d4bdd5e7dce79401366c6631af71cd2b1c44c5
Deborah Goossens <deborah@connectholland.nl>
2015-11-25 21:33:43 +0100

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,18 @@
+/*
+ * Client
+ */
+package timetracker;
+
+/**
+ *
+ * @author Deborah Goossens
+ */
+class Client {
+    public int id;
+    public String name;
+    
+    public Client(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
